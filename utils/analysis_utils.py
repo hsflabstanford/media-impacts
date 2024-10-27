96d3f5e36285852b754c34be74fb1eb820446391
thomasat <thomasat@stanford.edu>
2024-10-27 12:10:35 -0700

    move functions to analysis_utils

--- utils/analysis_utils.py
+++ utils/analysis_utils.py
@@ -350,6 +350,377 @@ def run_analysis(dct, df, trt_lag, non_add_L_set,
     
     return x_pval, x_beta, x_se, res
 
+# Run controls
+
+"""
+Positive and negative controls.
+Positive controls: (sum of TS albums) x each TS outcome
+Negative controls: 
+1) (each TS album + sum) x vegan recipes
+2) (each doc + sum) x TS outcomes
+
+Binary approach: 
+Y_t ~ I_t + L_t + L_t-1 + Y_t-1
+"""
+
+
+    
+
+def run_controls(df, pval_thresh=0.05, PS=False, PS_logistic=False, fit_method='OLS', model='lagged', bonf_denom=1,
+                add_L=True, difference=False, normalize=False, include_month=True, include_time=True,
+                verbose=False):
+    assert model in ['assoc', 'lagged', 'contemp']
+    if model == 'assoc':
+        assert add_L is False
+
+    # Positive controls #1
+    pval_thresh = 0.05/bonf_denom
+    total = 0
+    passed = 0
+    
+    trt_lag = 1 if (model == 'lagged') else 0
+    non_add_L_set = []
+    confounds = []
+    
+    for X in ['all_ts_albums']:
+        for Y in all_ts_outcomes:
+            df, analysis_dct = prepare_analysis_dct(df.copy(), X, Y, 
+                                                    trt_lag, 
+                                                    model, 
+                                                    add_L, include_time, 
+                                                    include_month, 
+                                                    confounds, non_add_L_set)
+            
+            x_pval, x_beta, _, _ = run_analysis(analysis_dct, df.copy(), trt_lag, non_add_L_set,
+                                                PS=PS, PS_logistic=PS_logistic,
+                                          fit_method=fit_method,
+                                         difference=difference, normalize=normalize,
+                                               verbose=verbose, include_time=False,
+                                               include_month=False)
+
+            #print(X, Y, x_pval)
+            if x_pval < pval_thresh:
+                passed += 1
+            total += 1
+    print('TS Pos Control tests success fraction: ', float(passed)/total, 'Passed: ', passed, 'Total: ', total)
+ 
+    # Positive controls #2
+    total = 0
+    passed = 0
+    
+    for X in ['all_drake_albums']:
+        for Y in all_drake_outcomes:
+            df, analysis_dct = prepare_analysis_dct(df.copy(), X, Y, 
+                                                    trt_lag, 
+                                                    model, 
+                                                    add_L, include_time, 
+                                                    include_month, 
+                                                    confounds, non_add_L_set)                
+            #for alb in all_ts_albums:
+            #    analysis_dct['C'].append((alb, 2))
+            x_pval, x_beta, _, _ = run_analysis(analysis_dct, df.copy(), trt_lag, non_add_L_set,
+                                                PS=PS,
+                                          PS_logistic=PS_logistic,
+                                          fit_method=fit_method,
+                                         difference=difference, normalize=normalize,
+                                               verbose=verbose, include_time=False,
+                                               include_month=False)
+
+            #print(X, Y, x_pval)
+
+            if x_pval < pval_thresh:
+                passed += 1
+            total += 1
+    print('Drake Pos Control tests success fraction: ', float(passed)/total, 'Passed: ', passed, 'Total: ', total)
+
+
+    # Negative controls #1
+    total1 = 0
+    passed1 = 0
+    
+    X_vals = all_ts_albums + ['all_ts_albums'] + all_drake_albums + ['all_drake_albums']
+    confounds = ['health', 'animal_welfare', 'all_climate']
+    for X in X_vals:
+        for Y in all_primary_outcomes:
+            df, analysis_dct = prepare_analysis_dct(df.copy(), X, Y, 
+                                                    trt_lag, 
+                                                    model, 
+                                                    add_L, include_time, 
+                                                    include_month, 
+                                                    confounds, non_add_L_set)  
+                   
+            x_pval, x_beta, _, _ = run_analysis(analysis_dct, df.copy(), trt_lag, 
+                                                non_add_L_set,
+                                                PS=PS, 
+                                          PS_logistic=PS_logistic,
+                                          fit_method=fit_method,
+                                         difference=difference, normalize=normalize,
+                                               verbose=verbose, include_time=False,
+                                               include_month=False)
+            if x_pval >= pval_thresh:
+                passed1 += 1
+            else:
+                print('failed')
+                print(X, Y, x_pval)
+            total1 += 1
+    print('Neg Control #1 tests success fraction: ', float(passed1)/total1, 'Passed: ', passed1, 'Total: ', total1)
+    
+    # Negative controls #2
+    # Add in the other docs as precision covariates?
+    total2 = 0
+    passed2 = 0
+    
+    
+    Y_vals = all_ts_outcomes + all_drake_outcomes
+    for X in all_docs + ['all_docs']:
+        for Y in Y_vals:
+            
+            df, analysis_dct = prepare_analysis_dct(df.copy(), X, Y, 
+                                                    trt_lag, 
+                                                    model, 
+                                                    add_L, include_time, 
+                                                    include_month, 
+                                                    confounds, non_add_L_set)  
+            
+            x_pval, x_beta, _, _ = run_analysis(analysis_dct, df.copy(), trt_lag, 
+                                                non_add_L_set,
+                                                PS=PS, 
+                                          PS_logistic=PS_logistic,
+                                          fit_method=fit_method,
+                                         difference=difference, normalize=normalize,
+                                               verbose=verbose, include_time=False,
+                                               include_month=False)
+
+            if x_pval >= pval_thresh:
+                passed2 += 1
+            else:
+                print('failed')
+                print(X, Y, x_pval)
+            total2 += 1
+            
+    print('Neg Control #2 success fraction: ', float(passed2)/total2, 'Passed: ',  passed2, 'Total: ', total2)
+    print('Neg Control success fraction: ', float(passed1 + passed2)/(total1 + total2), 'Passed: ',  passed1 + passed2, 'Total: ', total1 + total2)
+
+"""
+Models: assoc, lagged, contemp, bin.
+Assoc: Y_t ~ X_t
+Contemp: Y_t ~ PS_t + X_t + X_t-1 + Y_t-1 + L_t + L_t-1 + Month
+Lagged: Y_t ~ PS_t-1 + X_t-1 + X_t-2 + L_t-1 + L_t-2 + Y_t-2 + Month
+Bin: Y_t ~ PS + I_t + I_t*Time + Time + L_t + Month #+ Y_t-1 
+"""
+def run_primary_analyses(df, X, outcome, PS=False, PS_logistic=False, fit_method='OLS', model='lagged', 
+                         add_L=True, difference=False, include_time=False, 
+                         include_month=False, duration_months=None, normalize=False, verbose=False):
+    assert model in ['assoc', 'lagged', 'contemp', 'bin']
+    if model is 'assoc':
+        assert add_L is False
+        assert PS is False
+
+    if units == 'months':
+        other_docs = ['fok', 'cowspiracy', 'owth', 'tgc']
+
+    elif units == 'weeks':
+        other_docs = ['fok', 'cowspiracy', 'okja', 'wth', 'tgc']
+    elif units == 'days':
+        other_docs = ['fok', 'cowspiracy', 'okja', 'wth']
+    else:
+        assert 0
+    bin_other_docs = {'fok': [], 'cowspiracy': ['fok'], 'owth': ['fok', 'cowspiracy'],
+                      'okja': ['fok', 'cowspiracy'], 'wth': ['fok', 'cowspiracy'],
+                         'tgc': ['fok', 'cowspiracy', 'owth'],
+                     'yawye': ['fok', 'cowspiracy', 'owth', 'tgc']}
+        
+    if duration_months is not None:
+        assert model == 'bin'
+        init_date = df['ds'][0]
+        this_date = '{y}-{m}-{d}'.format(y=netflix_release_dates[X][0], 
+                                             m=netflix_release_dates[X][1], d=netflix_release_dates[X][2])
+
+        release_date_time = date_difference(init_date, this_date, units)
+        
+        if units == 'months':
+            offset = duration_months
+        elif units == 'weeks':
+            offset = duration_months*4
+        else:
+            assert 0
+        df = df.loc[df['Time'] <= release_date_time + offset]
+        
+        print('Intvn start time: ', df.loc[df[X + '_bin'] == 1].head()['ds'])
+        print('After truncating by duration_months: ', df.tail()['ds'])
+        
+    """
+    if combine_owth:
+        disaggr_docs = list(set(all_docs) - set(['okja', 'wth'])) + ['owth']
+    else:
+        disaggr_docs = list(set(all_docs)) 
+    """
+        
+    trt_lag = 1 if (model == 'lagged') else 0
+    #trt_lag = 2 if (model == 'lagged') else 0
+    
+    confounds = ['health', 'animal_welfare', 'all_climate']
+    trends_full_additional_confounds = ['food']
+    trends_inf_additional_confounds = ['informative_bare']
+    trends_behav_additional_confounds = ['behavior_bare']
+    consumption_additional_confounds = ['rdpi']
+    non_add_L_set = []
+
+    
+    category_dct = {'vegan': 'trends_full', 'vegetarian': 'trends_full', 
+                    'plant_based_plus_plant_based': 'trends_full', 
+                    'vegan_informative': 'trends_inf', 'vegetarian_informative': 'trends_inf',
+       'plant_based_informative': 'trends_inf', 
+                    'vegan_behavior': 'trends_behav', 'vegetarian_behavior': 'trends_behav',
+       'plant_based_behavior': 'trends_behav',
+                       'ksu_chicken': 'consumption', 'ksu_pork': 'consumption', 
+                    'ksu_beef': 'consumption', 'Zhao': 'consumption', 'NeuhoferLusk': 'consumption',
+                   'StewartMilk': 'consumption', 'StewartPBMilk': 'consumption'}
+    
+    if category_dct[outcome] == 'trends_full':
+        confounds += trends_full_additional_confounds
+    elif category_dct[outcome] == 'trends_inf':
+        confounds += trends_inf_additional_confounds
+    elif category_dct[outcome] == 'trends_behav':
+        confounds += trends_behav_additional_confounds        
+    else:
+        assert category_dct[outcome] == 'consumption'
+        confounds += consumption_additional_confounds
+            
+    #for X in disaggr_docs + ['all_docs']:
+    #, 
+    
+    analysis_dct = {'C': [], 'Ind_PS': []}
+    
+    if include_time:
+        #df['Time**2'] = df['Time']*df['Time']
+        analysis_dct['C'].append(('Time', [0]))
+        analysis_dct['Ind_PS'].append(('Time', [0]))
+        #analysis_dct['C'].append(('Time**2', [0]))
+        #analysis_dct['Ind_PS'].append(('Time**2', [0]))
+        
+    if include_month:
+        month_dummies = []
+        month_vals = sorted(df['Month'].unique())
+        for val_idx in range(len(month_vals)):
+            if val_idx == 0:
+                continue
+            this_val = month_vals[val_idx]
+            df['Month_Dummy' + str(this_val)] = (df['Month'] == this_val).astype(float)
+            month_dummies.append('Month_Dummy' + str(this_val))
+        
+        
+            analysis_dct['C'].append(('Month_Dummy' + str(this_val), [0]))
+            analysis_dct['Ind_PS'].append(('Month_Dummy' + str(this_val), [0]))
+    
+    
+    if model == 'lagged':
+        #"""
+        analysis_dct['X'] = (X, [trt_lag,trt_lag+1])
+        analysis_dct['Y'] = (outcome, [trt_lag+1])
+         
+        
+        for c in confounds:
+            if add_L and c not in non_add_L_set:
+                analysis_dct['C'].append((c, [trt_lag,trt_lag+1]))
+            else:
+                analysis_dct['C'].append((c, [trt_lag+1]))
+                
+                
+        analysis_dct['Dep_PS'] = (X, [0])
+        analysis_dct['Ind_PS'] += [(X, [1]), (outcome, [1])]
+
+        for c in confounds:
+            analysis_dct['Ind_PS'].append((c, [1]))          
+            if add_L:
+                analysis_dct['Ind_PS'].append((c, [0]))   
+
+    elif model == 'contemp':
+        analysis_dct['X'] = (X, [0,1])
+        analysis_dct['Y'] = (outcome, [1]) 
+        
+        for c in confounds:
+            if add_L and c not in non_add_L_set:
+                analysis_dct['C'].append((c, [0,1]))
+            else:
+                analysis_dct['C'].append((c, [1]))  
+                
+        analysis_dct['Dep_PS'] = (X, [0])
+        analysis_dct['Ind_PS'] += [(X, [1]), (outcome, [1])]
+
+        for c in confounds:
+            analysis_dct['Ind_PS'].append((c, [1]))          
+            if add_L:
+                analysis_dct['Ind_PS'].append((c, [0]))                 
+
+
+    elif model == 'assoc':
+        analysis_dct['X'] = (X, [0])
+        analysis_dct['Y'] = (outcome, [])   
+        
+    elif model == 'bin':
+        if not bin_slope:
+            #(X + '_bin*Time', [0])
+            analysis_dct['X'] = (X + '_bin', [0])
+            analysis_dct['Y'] = (outcome, [])
+            analysis_dct['C'].append((X + '_bin*Time_C', [0]))  
+            if add_L:
+                for c in confounds:
+                    analysis_dct['C'].append((c, [0]))
+                                        
+        else:
+            #(X + '_bin*Time', [0])
+            analysis_dct['X'] = (X + '_bin*Time_C', [0])
+            analysis_dct['Y'] = (outcome, [])
+            analysis_dct['C'].append((X + '_bin', [0]))   
+            if add_L:
+                for c in confounds:
+                    analysis_dct['C'].append((c, [0]))            
+                
+        # Specifically the PS regression
+        df[X + '_bin_orig'] = df[X + '_bin'].copy()
+        analysis_dct['Dep_PS'] = (X + '_bin_orig', [0])
+        analysis_dct['Ind_PS'] = []
+        if add_L:
+            for c in confounds:
+                analysis_dct['Ind_PS'].append((c, [0]))            
+    
+    # Add in confounds
+    if X != 'all_docs':
+        for other_doc in other_docs:
+            if other_doc == X:
+                continue
+            if model == 'lagged':
+                if add_L:
+                    analysis_dct['C'].append((other_doc, [trt_lag,trt_lag+1]))
+                else:
+                    analysis_dct['C'].append((other_doc, [trt_lag+1]))                    
+            elif model == 'contemp':
+                if add_L:
+                    analysis_dct['C'].append((other_doc, [0,1]))
+                else:
+                    analysis_dct['C'].append((other_doc, [1]))                
+            elif model == 'bin':
+                if other_doc in bin_other_docs[X]:
+                    if add_L:
+                        analysis_dct['C'].append((other_doc, [0]))
+            elif model == 'assoc':
+                pass
+
+
+    #print('analysis_dct: ', analysis_dct)
+            
+    x_pval, x_beta, x_se, _ = run_analysis(analysis_dct, df, trt_lag, non_add_L_set, 
+                                        PS=PS, PS_logistic=PS_logistic, fit_method=fit_method,
+                                  add_L=add_L,
+                                  difference=difference,
+                                  include_time=False,
+                                  include_month=False,
+                                  normalize=normalize,
+                                 verbose=verbose)          
+
+    return x_pval, x_beta, x_se
+
 def prepare_analysis_dct(df, X, Y, trt_lag, model, add_L, include_time, include_month, confounds, non_add_L_set):
     analysis_dct = {'C': [], 'Ind_PS': []}
     
